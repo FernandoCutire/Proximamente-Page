6061f996bddb22e88c00a1e1f76ea22f75beeff1
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-10-31 22:03:41 -0500

    Fixed typo in notificame cuando este lista

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Notifícame cuandone esté lista
+          Notifícame cuando esté lista
         </a>
       </header>
     </div>
